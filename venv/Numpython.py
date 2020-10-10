01855603f60c9b6c6bfd9481ee6dfe523c80ab5f
Your Name <yraghu13@gmail.com>
2020-10-10 19:00:33 +0200

    diff b/w the time of python list and numpy array

--- venv/Numpython.py
+++ venv/Numpython.py
@@ -1,7 +1,32 @@
-from numpy import *
+import numpy  as np
+import time
 
-ar = array([1,2,3,4,4,4], int)
-print(ar)
-ar = linspace(1,100,4)
+# ar = array([1,2,3,4,4,4], int)
+# print(ar)
+# ar = linspace(1,100,4)  creating list using linspace
+#
+# print(ar)
 
-print(ar)
+x = range(1000000)
+y = range(1000000, 2000000)
+
+
+
+# time taken to create python list
+start = time.time()
+z = [(x+y) for x,y in zip(x,y)]
+end = time.time()
+print("time took :", (end-start)*1000 , 'z')
+
+# a = np.array([[1, 2, 3], [4, 5, 6]]) ----- numpy array
+# print(a)
+#
+# b= np.array([4,6,7])
+# print('b:' ,b)
+
+
+# time taken by numpy array
+start = time.time()
+c = np.arange(1000000)
+end = time.time()
+print("time took :", (end-start)*1000 , 'c')
