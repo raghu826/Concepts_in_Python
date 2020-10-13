ca214f6d7d276111668d44005466c2f10bffb393
Your Name <yraghu13@gmail.com>
2020-10-13 18:04:19 +0200

    added comments

--- Exercise/MostRepeatd.py
+++ Exercise/MostRepeatd.py
@@ -1,14 +1,18 @@
 # to find out the most repeated character in the text
 
 text = "fbhwkajfrfbuvnifvubrs"
-count = {}
+count = {}   # creat a dictionary
 for i in text:
-    if i in count:
+    if i in count:          # if char is already in the dict then increment count by 1
         count[i] += 1
-    else:
+    else:                   # if not make it as 1
         count[i] = 1
 
 count_sort = sorted(count.items(), key = lambda kv :kv[1], reverse = True)
+
+# items() func makes dict into list of tuples[()()()()]
+# key is used to sort by value
+# to reverse the list,  reverse = True
 
 print(count)
 print(count_sort[0])
