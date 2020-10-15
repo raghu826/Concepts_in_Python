5df1b96d1064b3b99d9a0ad4b921fa962420714b
Your Name <yraghu13@gmail.com>
2020-10-15 23:43:22 +0200

    updated decorator

--- Decorators/Decorate.py
+++ Decorators/Decorate.py
@@ -1,16 +1,14 @@
 import time
 
-# def update(func):
-#     def inner(*args, **kwargs):
-#         func(*args, **kwargs)
-#
-#     return inner
-#
-# @update
-# def something(num):
-#     print("Hello world", num)
-#
-# something(2)
+
+def update(func):
+    def inner(*args, **kwargs):
+        func(*args, **kwargs)
+    return inner
+
+@update
+def smthng(num):
+    print("Hello world", num)
 
 # ****************************************************
 
@@ -22,7 +20,7 @@ def time_it(func):
         func()
         end = time.time()
         print(end)
-        print(f"total time is {(end-start)*1000} in milli secs")
+        print(f"total time is {(end-start)*1000}  milli secs")
     return wrapper
 
 
@@ -33,4 +31,6 @@ def something():
             print(i)
 
 
-something()
+if __name__ == '__main__':
+    smthng(2)
+    something()
